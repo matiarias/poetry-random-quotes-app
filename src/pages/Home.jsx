1e3726588232b9c5fc834e2d09b613af9bfd762a
Matias Arias <matt.arias182@gmail.com>
2022-08-30 01:55:04 -0300

    se creo función para los dos botones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../components/footer/Footer";
 
 const Home = () => {
+  const [quotes, setQuotes] = useState("");
+
+  const quotesApi = async () => {
+    try {
+      const resp = await fetch("https://api.adviceslip.com/advice");
+      const { slip } = await resp.json();
+      const { advice } = slip;
+      setQuotes(advice);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    quotesApi();
+  }, []);
+
+  const handleClick = () => {
+    quotesApi();
+    console.log(quotes);
+  };
+
+  const copyText = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("texto copiado");
+    } catch (error) {
+      console.log("error al copiar el texto");
+    }
+  };
+
+  const handleClickCopyText = () => {
+    copyText(quotes);
+  };
+
   return (
     <>
-      <div className="h-screen w-full"></div>
+      <div className="h-screen w-full flex flex-col justify-center items-center gap-16">
+        <div className="flex gap-12">
+          <button
+            onClick={handleClick}
+            type="button"
+            className="p-2 md:py-2 md:px-4 bg-green-800/60 text-white rounded-lg text-base md:text-xl border-2 border-gray-500"
+          >
+            Dame una frase
+          </button>
+          <button
+            onClick={handleClickCopyText}
+            type="button"
+            className="p-2 md:py-2 md:px-4 bg-yellow-500/50 text-white rounded-lg text-base md:text-xl border-2 border-gray-500"
+          >
+            Copiar
+          </button>
+        </div>
+        <div className="h-auto p-4 bg-gray-900/60 rounded-lg border-2 border-indigo-800 shadow-md shadow-gray-200">
+          <p className="text-white font-bold text-xl md:text-2xl">{quotes}</p>
+        </div>
+      </div>
       <Footer />
     </>
   );
